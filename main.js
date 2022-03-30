f0ab437639355d31c3caeaafded0d1d5c0763c97
bossteeh <akinrinolaelijah@gmail.com>
2022-03-30 05:51:16 +0100

    added javascript to select star rating

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*
 document.getElementById('starone').onclick = function() {
     document.getElementById('starone').style.color = "rgb(252, 177, 38)"
 
@@ -17,6 +18,48 @@ document.getElementById('starfour').onclick = function() {
     document.getElementById('startwo').style.color = "rgb(252, 177, 38)"
     document.getElementById('starthree').style.color = "rgb(252, 177, 38)"
     document.getElementById('starfour').style.color = "rgb(252, 177, 38)"
+}
+document.getElementById('starfive').onclick = function() {
+    document.getElementById('starone').style.color = "rgb(252, 177, 38)"
+    document.getElementById('startwo').style.color = "rgb(252, 177, 38)"
+    document.getElementById('starthree').style.color = "rgb(252, 177, 38)"
+    document.getElementById('starfour').style.color = "rgb(252, 177, 38)"
+    document.getElementById('starfive').style.color = "rgb(252, 177, 38)"
+
+}
+
+*/
+document.getElementById('starone').onclick = function() {
+    document.getElementById('starone').style.color = "rgb(252, 177, 38)"
+    document.getElementById('startwo').style.color = "black"
+    document.getElementById('starthree').style.color = "black"
+    document.getElementById('starfour').style.color = "black"
+    document.getElementById('starfive').style.color = "black"
+
+}
+document.getElementById('startwo').onclick = function() {
+    document.getElementById('starone').style.color = "rgb(252, 177, 38)"
+    document.getElementById('startwo').style.color = "rgb(252, 177, 38)"
+    document.getElementById('starthree').style.color = "black"
+    document.getElementById('starfour').style.color = "black"
+    document.getElementById('starfive').style.color = "black"
+
+}
+document.getElementById('starthree').onclick = function() {
+    document.getElementById('starone').style.color = "rgb(252, 177, 38)"
+    document.getElementById('startwo').style.color = "rgb(252, 177, 38)"
+    document.getElementById('starthree').style.color = "rgb(252, 177, 38)"
+    document.getElementById('starfour').style.color = "black"
+    document.getElementById('starfive').style.color = "black"
+
+}
+document.getElementById('starfour').onclick = function() {
+    document.getElementById('starone').style.color = "rgb(252, 177, 38)"
+    document.getElementById('startwo').style.color = "rgb(252, 177, 38)"
+    document.getElementById('starthree').style.color = "rgb(252, 177, 38)"
+    document.getElementById('starfour').style.color = "rgb(252, 177, 38)"
+    document.getElementById('starfive').style.color = "black"
+
 }
 document.getElementById('starfive').onclick = function() {
     document.getElementById('starone').style.color = "rgb(252, 177, 38)"
