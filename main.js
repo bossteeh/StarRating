4d309f0cd88e85dcdc3d356d8b893cd4c871e6d8
bossteeh <akinrinolaelijah@gmail.com>
2022-03-30 05:12:07 +0100

    interface design in progress

---
+++ main.js
@@ -0,0 +1,28 @@
+document.getElementById('starone').onclick = function() {
+    document.getElementById('starone').style.color = "rgb(252, 177, 38)"
+
+}
+
+document.getElementById('startwo').onclick = function() {
+    document.getElementById('starone').style.color = "rgb(252, 177, 38)"
+    document.getElementById('startwo').style.color = "rgb(252, 177, 38)"
+}
+document.getElementById('starthree').onclick = function() {
+    document.getElementById('starone').style.color = "rgb(252, 177, 38)"
+    document.getElementById('startwo').style.color = "rgb(252, 177, 38)"
+    document.getElementById('starthree').style.color = "rgb(252, 177, 38)"
+}
+document.getElementById('starfour').onclick = function() {
+    document.getElementById('starone').style.color = "rgb(252, 177, 38)"
+    document.getElementById('startwo').style.color = "rgb(252, 177, 38)"
+    document.getElementById('starthree').style.color = "rgb(252, 177, 38)"
+    document.getElementById('starfour').style.color = "rgb(252, 177, 38)"
+}
+document.getElementById('starfive').onclick = function() {
+    document.getElementById('starone').style.color = "rgb(252, 177, 38)"
+    document.getElementById('startwo').style.color = "rgb(252, 177, 38)"
+    document.getElementById('starthree').style.color = "rgb(252, 177, 38)"
+    document.getElementById('starfour').style.color = "rgb(252, 177, 38)"
+    document.getElementById('starfive').style.color = "rgb(252, 177, 38)"
+
+}
