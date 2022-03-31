1cbf3273496f6f232eb2e81ae211de2e2ebf34c8
bossteeh <akinrinolaelijah@gmail.com>
2022-03-31 01:59:32 +0100

    added emoji to make it more fun to  rate

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ document.getElementById('starone').onclick = function () {
     document.getElementById('starthree').style.color = "black"
     document.getElementById('starfour').style.color = "black"
     document.getElementById('starfive').style.color = "black"
+    document.getElementById('emoji').innerHTML = "&#128545";
     document.getElementById('response').innerHTML = 1;
     console.log(1)
     localStorage.setItem("rank", 1)
@@ -19,6 +20,7 @@ document.getElementById('startwo').onclick = function () {
     document.getElementById('starfour').style.color = "black"
     document.getElementById('starfive').style.color = "black"
     document.getElementById('response').innerHTML = 2;
+    document.getElementById('emoji').innerHTML = "&#128543";
     console.log(2)
     localStorage.setItem("rank", 2)
 
@@ -31,6 +33,7 @@ document.getElementById('starthree').onclick = function () {
     document.getElementById('starfour').style.color = "black"
     document.getElementById('starfive').style.color = "black"
     document.getElementById('response').innerHTML = 3;
+    document.getElementById('emoji').innerHTML = "&#128528";
     console.log(3)
     localStorage.setItem("rank", 3)
 
@@ -43,6 +46,7 @@ document.getElementById('starfour').onclick = function () {
     document.getElementById('starfour').style.color = "rgb(252, 177, 38)"
     document.getElementById('starfive').style.color = "black"
     document.getElementById('response').innerHTML = 4;
+    document.getElementById('emoji').innerHTML = "&#128578";
     console.log(4)
     localStorage.setItem("rank", 4)
 }
@@ -53,6 +57,7 @@ document.getElementById('starfive').onclick = function () {
     document.getElementById('starfour').style.color = "rgb(252, 177, 38)"
     document.getElementById('starfive').style.color = "rgb(252, 177, 38)"
     document.getElementById('response').innerHTML = 5;
+    document.getElementById('emoji').innerHTML = "&#128522";
     console.log(5)
     localStorage.setItem("rank", 5)
 
